22e09e5dcc81d76eee7c038f185109253391b7b3
Giga Samkharadze <gigasamkharadzee@gmail.com>
2024-04-15 00:24:14 +0400

    feat: requests for racing

--- async-race/src/components/Car.tsx
+++ async-race/src/components/Car.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useGarage } from '../context/CarContext.tsx';
 
 interface CarProps {
@@ -11,10 +11,43 @@ interface CarProps {
 function Car({
   name, color, id, setSelectedCar,
 }: CarProps) {
-  const [isAnimated, setIsAnimated] = useState(false);
   const { refetchGarage } = useGarage();
+  const [status, setStatus] = useState('stopped');
+  const [isAnimating, setIsAnimating] = useState(false);
+  const carRef = useRef<HTMLImageElement>(null);
 
-  console.log(name, color, id);
+  function startAnimation(velocity: number, distance: number) {
+    carRef.current?.style.setProperty('--animation-duration', `${distance / velocity}s`);
+    setIsAnimating(true);
+  }
+
+  function stopAnimation() {
+    setIsAnimating(false);
+  }
+
+  function handleStop() {
+    setStatus('stopped');
+    stopAnimation();
+  }
+
+  function handleStart() {
+    fetch(`http://127.0.0.1:3000/engine?id=${id}&status=started`, {
+      method: 'PATCH',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setStatus('started');
+        startAnimation(data.velocity, data.distance);
+        fetch(`http://127.0.0.1:3000/engine?id=${id}&status=started`, {
+          method: 'PATCH',
+        }).then((response) => {
+          if (response.status !== 200) {
+            setStatus('stopped');
+            handleStop();
+          }
+        });
+      });
+  }
 
   return (
     <div className="parent-container w-full flex gap-3 items-center">
@@ -40,27 +73,31 @@ function Car({
       </div>
       <div className="flex flex-col text-white gap-1">
         <button
-          onClick={() => {
-            setIsAnimated(true);
-            setTimeout(() => setIsAnimated(false), 3000);
-          }}
+          onClick={handleStart}
           type="button"
           className="bg-white rounded-sm p-1 hover:bg-gray-100 text-black"
+          disabled={isAnimating}
         >
           A
         </button>
         <button
-          onClick={() => setIsAnimated(false)}
+          onClick={handleStop}
           type="button"
           className="bg-white rounded-sm p-1 hover:bg-gray-100 text-black"
+          disabled={!isAnimating}
         >
           B
         </button>
       </div>
       <img
-        src="racing-car.png"
+        src="racing-car.svg"
         alt="racing car"
-        className={`car ${isAnimated ? 'animated-object' : ''}`}
+        className={`car ${isAnimating ? 'animate' : ''}`}
+        ref={carRef}
+        onAnimationEnd={() => {
+          stopAnimation();
+          setStatus('finished');
+        }}
       />
       <p className="ml-48 text-white">{name}</p>
     </div>
